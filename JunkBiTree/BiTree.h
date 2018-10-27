098935b8589f02143b7e1e7b0c01cee723fce148
Jim Sun <x739566858@outlook.com>
2018-10-27 18:39:06 +0800

    BiTree Contiuned

--- JunkBiTree/BiTree.h
+++ JunkBiTree/BiTree.h
@@ -18,3 +18,26 @@ typedef struct BiTree {
 	BiTreeNode *root;
 	BiTree *next;
 }BiTree;
+
+//APIs
+
+status		InitBiTree(BiTree &T);
+status		DestroyBiTree(BiTree &T);
+status		CreateBiTree(BiTree &T, int length, ElemType *preorder, ElemType *inorder);
+status		ClearBiTree(BiTree &T);
+bool		BiTreeEmpty(const BiTree &T);
+int			BiTreeDepth(const BiTree &T);
+BiTreeNode *Root(const BiTree &T);
+status		Value(const BiTree &T, BiTreeNode &e);
+status		Assign(BiTree &T, BiTreeNode &e,ElemType &value);
+BiTreeNode *Parent(const BiTree &T, BiTreeNode &e);
+BiTreeNode *LeftChild(const BiTree &T, BiTreeNode &e);
+BiTreeNode *RightChild(const BiTree &T, BiTreeNode &e);
+BiTreeNode *LeftSibling(const BiTree &T, BiTreeNode &e);
+BiTreeNode *RightSibling(const BiTree &T, BiTreeNode &e);
+status      InsertChild(BiTree &T, BiTreeNode &p, int LR, BiTree &c);
+status      DeleteChild(BiTree &T, BiTreeNode &p, int LR);
+status      PreOrderTraverse(const BiTree &T);
+status      InOrderTraverse(const BiTree &T);
+status      PostOrderTraverse(const BiTree &T);
+status		LevelOrderTraverse(const BiTree &T);
